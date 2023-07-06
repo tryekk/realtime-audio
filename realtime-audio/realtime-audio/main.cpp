161ce7834c9f133ce7cdd055b39cccddb21c5955
Tom Carter <31577520+tryekk@users.noreply.github.com>
2023-07-06 21:49:49 +0100

    Initial commit

---
+++ realtime-audio/realtime-audio/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  realtime-audio
+//
+//  Created by Tom Carter on 06/07/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
